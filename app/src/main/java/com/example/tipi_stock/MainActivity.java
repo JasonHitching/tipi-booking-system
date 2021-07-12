95162c6653c5265d95073e470349ab7fbdf7a233
Jason Hitching <jasonhitching1994@gmail.com>
2021-07-12 20:50:34 +0100

    Bottom navigation implemented, fragment transitions implemented

--- app/src/main/java/com/example/tipi_stock/MainActivity.java
+++ app/src/main/java/com/example/tipi_stock/MainActivity.java
@@ -1,42 +1,35 @@
 package com.example.tipi_stock;
 
+import android.os.Bundle;
+
+import com.example.tipi_stock.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import android.os.Bundle;
-
-import com.example.tipi_stock.ui.bookings.BookingFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // condition used to only render each fragment once
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .setReorderingAllowed(true)
-                    .add(R.id.fragment_container_view, BookingFragment.class, null)
-                    .commit();
-        }
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.bottom_nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.home_nav, R.id.booking_nav, R.id.inventory_nav, R.id.logout_nav)
                 .build();
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.fragment_container_view);
-        NavController navController = Navigation.findNavController(this, R.id.fragment_container_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(null, navController);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
+
 }
