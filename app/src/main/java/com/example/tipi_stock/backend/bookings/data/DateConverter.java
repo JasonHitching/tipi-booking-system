0a9d254653cc114de8a2a54b0cdd0b743843df8c
Jason Hitching <jasonhitching1994@gmail.com>
2021-07-20 20:03:51 +0100

    <implemented> Type converter for storing Date objects in Room persistance library

---
+++ app/src/main/java/com/example/tipi_stock/backend/bookings/data/DateConverter.java
@@ -0,0 +1,40 @@
+package com.example.tipi_stock.backend.bookings.data;
+
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+
+/**
+ * Class for converting a Long value to a Date and vice versa
+ * The Room persistence library knows how to persist Long objects but not Date objects
+ */
+public class DateConverter {
+
+    /**
+     * Type converter for converting a timestamp to a Date object
+     * @param timestamp
+     * @return a new Date object
+     */
+    @TypeConverter
+    public static Date convertFromTimestamp(Long timestamp) {
+        if (timestamp == null) {
+            return null;
+        } else {
+            return new Date(timestamp);
+        }
+    }
+
+    /**
+     * Type converter for converting a date to a timestamp
+     * @param date
+     * @return
+     */
+    @TypeConverter
+    public static Long convertDateToTimestamp(Date date) {
+        if (date == null) {
+            return null;
+        } else {
+            return date.getTime();
+        }
+    }
+}
