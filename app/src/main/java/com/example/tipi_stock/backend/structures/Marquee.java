f377a95b74b2eceed0312fc8e683d290a6d952ff
Jason Hitching <jasonhitching1994@gmail.com>
2021-07-26 09:45:13 +0100

    <finalised> Tipi and Marquee class|

--- app/src/main/java/com/example/tipi_stock/backend/structures/Marquee.java
+++ app/src/main/java/com/example/tipi_stock/backend/structures/Marquee.java
@@ -1,5 +1,10 @@
 package com.example.tipi_stock.backend.structures;
 
+import androidx.annotation.Nullable;
+
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Class for modelling a marquee object
  */
@@ -78,5 +83,49 @@ public class Marquee {
      */
     public int getCenterLegQuantity() {
         return centerLegQuantity;
+    }
+
+    /**
+     * Override the equals method to return a boolean that
+     * symbolises whether two objects are identical
+     *
+     * @param object object for comparison
+     * @return boolean value representing whether the two objects are equal
+     */
+    @Override
+    public boolean equals(@Nullable @org.jetbrains.annotations.Nullable Object object) {
+        // If the object isn't even an instance of Tipi return false immediately
+        if (!(object instanceof Marquee)) {
+            return false;
+        } else {
+            Marquee tempMarquee =  (Marquee) object;
+
+            // Compare all of object properties
+            return Objects.equals(this.clevisPinsQuantity, tempMarquee.clevisPinsQuantity)
+                    && Arrays.equals(this.components, tempMarquee.components)
+                    && Objects.equals(this.fairyLightQuantity, tempMarquee.fairyLightQuantity)
+                    && Objects.equals(this.largeGroundStakeQuantity, tempMarquee.largeGroundStakeQuantity)
+                    && Objects.equals(this.legQuantity, tempMarquee.legQuantity)
+                    && Objects.equals(this.mediumGroundStakeQuantity, tempMarquee.mediumGroundStakeQuantity)
+                    && Objects.equals(this.centerLegQuantity, tempMarquee.centerLegQuantity);
+        }
+    }
+
+    /**
+     * Override 'hashCode' for consistent storage in Hash date structures
+     * 
+     * @return hash of all object properties
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                this.components,
+                this.clevisPinsQuantity,
+                this.fairyLightQuantity,
+                this.largeGroundStakeQuantity,
+                this.legQuantity,
+                this.mediumGroundStakeQuantity,
+                this.centerLegQuantity
+        );
     }
 }
--- app/src/main/java/com/example/tipi_stock/backend/structures/Tipi.java
+++ app/src/main/java/com/example/tipi_stock/backend/structures/Tipi.java
@@ -1,20 +1,22 @@
 package com.example.tipi_stock.backend.structures;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 /**
  * Class for modelling a Tipi structure
  */
 public class Tipi {
 
-    private final int legQuantity;
-    private final int mediumGroundStakeQuantity;
-    private final int largeGroundStakeQuantity;
-    private final int fairyLightQuantity;
-    private final int clevisPinsQuantity;
-    private final String[] components;
+    private int legQuantity;
+    private int mediumGroundStakeQuantity;
+    private int largeGroundStakeQuantity;
+    private int fairyLightQuantity;
+    private int clevisPinsQuantity;
+    private String[] components;
 
     public Tipi() {
         components = new String[]{"Canvas", "8m Legs", "Large Ground Stakes", "Fairy Lights",
@@ -28,6 +30,7 @@ public class Tipi {
 
     /**
      * Leg quantity accessor
+     *
      * @return leg quantity per Tipi
      */
     public int getLegQuantity() {
@@ -36,6 +39,7 @@ public class Tipi {
 
     /**
      * Medium ground stake quantity accessor
+     *
      * @return medium ground stake quantity per Tipi
      */
     public int getMediumGroundStakeQuantity() {
@@ -44,6 +48,7 @@ public class Tipi {
 
     /**
      * Large ground stake quantity accessor
+     *
      * @return large ground stake quantity per Tipi
      */
     public int getLargeGroundStakeQuantity() {
@@ -52,6 +57,7 @@ public class Tipi {
 
     /**
      * Fairy light quantity accessor
+     *
      * @return fairy light quantity per Tipi
      */
     public int getFairyLightQuantity() {
@@ -60,6 +66,7 @@ public class Tipi {
 
     /**
      * Clevis pin quantity accessor
+     *
      * @return clevis pin quantity per Tipi
      */
     public int getClevisPinsQuantity() {
@@ -68,6 +75,7 @@ public class Tipi {
 
     /**
      * Components array accessor
+     *
      * @return components array
      */
     public String[] getComponents() {
@@ -77,6 +85,7 @@ public class Tipi {
     /**
      * Overridden toString method, returning a string
      * representation of the class instance fields
+     *
      * @return string representation of class
      */
     @NonNull
@@ -90,5 +99,46 @@ public class Tipi {
                 ", clevisPinsQuantity=" + clevisPinsQuantity +
                 ", components=" + Arrays.toString(components) +
                 '}';
+    }
+
+    /**
+     * Override the equals method to return a boolean that
+     * symbolises whether two objects are identical
+     *
+     * @param object object for comparison
+     * @return boolean value representing whether the two objects are equal
+     */
+    @Override
+    public boolean equals(@Nullable @org.jetbrains.annotations.Nullable Object object) {
+        // If the object isn't even an instance of Tipi return false immediately
+        if (!(object instanceof Tipi)) {
+            return false;
+        } else {
+            Tipi tempTipi =  (Tipi) object;
+
+            // Compare all of object properties
+            return Objects.equals(this.clevisPinsQuantity, tempTipi.clevisPinsQuantity)
+                    && Arrays.equals(this.components, tempTipi.components)
+                    && Objects.equals(this.fairyLightQuantity, tempTipi.fairyLightQuantity)
+                    && Objects.equals(this.largeGroundStakeQuantity, tempTipi.largeGroundStakeQuantity)
+                    && Objects.equals(this.legQuantity, tempTipi.legQuantity)
+                    && Objects.equals(this.mediumGroundStakeQuantity, tempTipi.mediumGroundStakeQuantity);
+        }
+    }
+
+    /**
+     * Override 'hashCode' for consistent storage in Hash date structures
+     * @return hash of all object properties
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                this.components,
+                this.clevisPinsQuantity,
+                this.fairyLightQuantity,
+                this.largeGroundStakeQuantity,
+                this.legQuantity,
+                this.mediumGroundStakeQuantity
+        );
     }
 }
