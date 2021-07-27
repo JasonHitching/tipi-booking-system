0b3d9c44ed3e3b685fbb313bd29920343d611933
Jason Hitching <jasonhitching1994@gmail.com>
2021-07-27 10:27:52 +0100

    <implemented> firebase signup functionality

--- app/src/main/java/com/example/tipi_stock/ui/register/RegisterFragment.java
+++ app/src/main/java/com/example/tipi_stock/ui/register/RegisterFragment.java
@@ -11,28 +11,70 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.tipi_stock.MainActivity;
 import com.example.tipi_stock.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+/**
+ * 
+ */
 public class RegisterFragment extends Fragment {
 
-    private RegisterViewModel viewModel;
+    private FirebaseAuth authenticator;
+    private Button registerButton;
+    private TextInputLayout emailTextInput, passwordTextInput;
+    private View rootView;
 
-    public static RegisterFragment newInstance() {
-        return new RegisterFragment();
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.register_fragment, container, false);
+        rootView = inflater.inflate(R.layout.register_fragment, container, false);
+        return rootView;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        viewModel = new ViewModelProvider(this).get(RegisterViewModel.class);
-        // TODO: Use the ViewModel
+        registerButton = rootView.findViewById(R.id.register_button);
+        emailTextInput = rootView.findViewById(R.id.email_text_field);
+        passwordTextInput = rootView.findViewById(R.id.password_text_field);
+
+        authenticator = FirebaseAuth.getInstance();
+        String emailText = emailTextInput.getEditText().getText().toString();
+        String passwordText = passwordTextInput.getEditText().getText().toString();
+        registerButton.setOnClickListener( registerButton -> {
+                createNewAccount(emailText, passwordText);
+        });
+
     }
 
-
+    /**
+     * Method for attempting to sign up a new user with firebase cloud storaged
+     *
+     * @param newEmail user inputted email
+     * @param newPassword user inputted password
+     */
+    private void createNewAccount(String newEmail, String newPassword) {
+        authenticator.createUserWithEmailAndPassword(newEmail, newPassword)
+                .addOnCompleteListener(requireActivity(), (OnCompleteListener<AuthResult>) signupTask -> {
+                    // Authentication was successful
+                    if (signupTask.isSuccessful()) {
+                        FirebaseUser user = authenticator.getCurrentUser();
+                        // Update user interface here
+                    } else {
+                        // Authentication failed with the firebase DB
+                        Toast.makeText(getActivity(), "Authentication failed!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
